c8f37139593927fb41317c0723f7de1046deccd8
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-15 11:55:47 +0000

    Clang Format && Clang Tidy configuration

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
-#include <memory>
 #include <iostream>
+#include <memory>
 using namespace std;
 #include "CPlayer.h"
 // #include "CGame.h"
@@ -9,7 +9,7 @@ int main()
     // unique_ptr<CGame> currentGame = make_unique<CGame>();
     unique_ptr<CPlayer> player1 = make_unique<CPlayer>("Player 1", 100, 100, 1);
     unique_ptr<CPlayer> player2 = make_unique<CPlayer>("Player 2", 100, 100, 1);
- 
+
     // Add players and spaces to the game
     cout << "Adding players and spaces to the game" << endl;
     cout << "Player 1: " << *player1 << endl;
