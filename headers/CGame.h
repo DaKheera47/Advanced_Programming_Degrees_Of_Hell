655a1b48480828af3f82c3beebeafb04dd4d50ba
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-20 17:40:27 +0000

    using typedef things for CPlayerVector

--- headers/CGame.h
+++ headers/CGame.h
@@ -1,35 +1,38 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "CPlayer.h"
 #include "CSpace.h"
 #include "CSpinner.h"
-#include <vector>
-#include <memory>
+
+using CPlayerVector = std::vector<std::unique_ptr<CPlayer>>;
 
 class CGame
 {
 private:
     CSpinner spinner;
-    std::vector<std::unique_ptr<CPlayer>> players;
-    std::vector<CSpace *> spaces;
+    CPlayerVector players;
+    std::vector<CSpace*> spaces;
 
 public:
     // Getters
-    std::vector<std::unique_ptr<CPlayer>> getPlayers();
-    std::vector<CSpace *> getSpaces();
+    CPlayerVector getPlayers();
+    std::vector<CSpace*> getSpaces();
 
     // Adding players
     void addPlayer(std::unique_ptr<CPlayer> player);
-    void addPlayer(const std::vector<std::unique_ptr<CPlayer>> &players);
+    void addPlayer(const CPlayerVector& players);
 
     // Adding spaces
-    void addSpace(CSpace *space);
-    void addSpace(const std::vector<CSpace *> &spaces);
+    void addSpace(CSpace* space);
+    void addSpace(const std::vector<CSpace*>& spaces);
 
     // Start game
     void start();
 
     // constructor
     CGame();
-    CGame(const std::vector<std::unique_ptr<CPlayer>> &players, const std::vector<CSpace *> &spaces);
+    CGame(const CPlayerVector& players, const std::vector<CSpace*>& spaces);
 };
