db8d40c06d5556836e645ba87e5be0354d229da8
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-23 14:44:13 +0000

    CSkipClasses implemented

--- source/spaces/CSkipClasses.cpp
+++ source/spaces/CSkipClasses.cpp
@@ -2,7 +2,7 @@
 
 void CSkipClasses::playerLanded(std::shared_ptr<CPlayer>& player, std::unique_ptr<CBoard>& board)
 {
-    // print the player's name and the space they landed on with somethign that says it's from the
-    // assessment space
-    std::cout << player->getName() << " landed on " << getName() << " (CSkipClasses)" << std::endl;
+    // message: <Player> lands on Skip Classes and hangs out with their dodgy mate
+    std::cout << player->getName() << " lands on Skip Classes and hangs out with their dodgy mate"
+              << std::endl;
 }
