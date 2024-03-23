259f5e404403e9bccbb025e560339300bd9fa5e5
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-23 23:06:25 +0000

    slight visual cleanup

--- source/CUtils.cpp
+++ source/CUtils.cpp
@@ -17,14 +17,17 @@ std::vector<std::string> CUtils::readFileLines(const std::string& filePath)
     std::ifstream file(filePath);
     std::vector<std::string> lines;
     std::string line;
+
     if (!file.is_open())
     {
         throw std::runtime_error("Could not open file: " + filePath);
     }
+
     while (getline(file, line))
     {
         lines.push_back(line);
     }
+
     return lines;
 }
 
