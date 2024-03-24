27bf9b23e9478bd87d8e5fc5675b520748b0e64f
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-24 15:26:18 +0000

    more things are contants now

--- headers/Constants.h
+++ headers/Constants.h
@@ -3,5 +3,8 @@
 class Constants
 {
 public:
-    static const int TOTAL_ROUNDS = 10;
+    static const int TOTAL_ROUNDS = 100;
+    static const int WW_MOTIV_INC = 250;
+    static const int ASSESSMENTS_PER_YEAR = 3;
+    static const int MAX_YEARS = 3;
 };
