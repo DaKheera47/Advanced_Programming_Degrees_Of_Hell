beef75216af4ec0c4b6c53d493c2ae48e122e16b
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-25 03:58:08 +0000

    typo fixed

--- source/spaces/CSkipClasses.cpp
+++ source/spaces/CSkipClasses.cpp
@@ -3,6 +3,6 @@
 void CSkipClasses::PlayerLanded(std::shared_ptr<CPlayer>& player, std::unique_ptr<CBoard>& board)
 {
     // message: <Player> lands on Skip Classes and hangs out with their dodgy mate
-    std::cout << player->GetName() << " lands on Skip Classes and hangs out with their dodgy mate"
+    std::cout << player->GetName() << " lands on Skip Classes and hangs out with their dodgy mates"
               << std::endl;
 }
